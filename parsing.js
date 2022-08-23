c3546ea73b6168a5dc89b9115f92521fe76a7bc3
azizjon <aliqulovazizjon68@gmail.com>
2022-08-23 08:54:48 +0500

    parser qismi tugadi endi botni strukturasini qilamiz va bugun yoki ertagacha tugata olamiz

--- parsing.js
+++ parsing.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cli = require("cli-color");
 const fs = require("fs");
-// headers qismini yozish
 const headers = {
   headers: {
     "User-Agent":
@@ -15,6 +15,57 @@ const linkOl = async (params, start = 1) => {
   //   if// data qismini yozish
   return data.data;
 };
-const parseData = async (data) => {};
 
-module.exports = { linkOl, parseData };
+const musicParser = async (name) => {
+  const html = await linkOl(name);
+  const $ = cheerio.load(html);
+  const soni = $(".berrors").text().trim().split(" ")[4] * 1;
+  if (!soni) {
+    throw new Error("Dasturda qo'shiqlar topilmadi");
+  }
+  const forNum = Math.floor(soni / 20) + 1;
+  console.log(forNum);
+  let info = {};
+  let arr = [];
+  for (let i = 1; i <= forNum; i++) {
+    const html1 = await linkOl(name, i);
+    const $$ = cheerio.load(html1);
+    $$(".play-desc").each((_, e) => {
+      info.name = $$(e).text().trim();
+      info.url = $$(e).attr().href;
+      arr.push(info);
+      info = {};
+    });
+  }
+
+  fs.writeFileSync("json.json", JSON.stringify(arr), () => {});
+  console.log(arr.length);
+  return arr;
+};
+
+let infoUrl = async (url, name = "download") => {
+  let html = await axios.get(url);
+  if (html) {
+    console.log(cli.redBright("info urlda xato bor"));
+  }
+  const $ = cheerio.load(html.data);
+  const data = $(".fbtn.fx-row.fx-middle.fdl");
+  if (!data) {
+    console.log(cli.redBright("biz infoUrl funksiyasidan urlni topolmadik"));
+  }
+  let url1 = data.attr().href;
+
+  console.log(url1);
+  let response = await axios.get(url1, { responseType: "stream" });
+  await response.data.pipe(fs.createWriteStream(`${name}.mp3`));
+};
+
+const parseData = async (data) => {
+  let info = await musicParser("Doxxim");
+  if (!info) {
+    throw new Error("hech qanday ma'lumot topilmadi");
+  }
+  let malumot = await infoUrl(info[2].url, info[2].name);
+};
+
+module.exports = { linkOl, parseData, infoUrl, musicParser };
